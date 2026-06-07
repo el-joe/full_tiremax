8755287ec628cf668ff31a0542c388cc4367c8c2
eng-saeed <eng.saeedsayed@gmail.com>
2026-06-07 20:55:28 +0300

    handel hero section responsive

--- frontend/components/pages/home/FoundTireBox.tsx
+++ frontend/components/pages/home/FoundTireBox.tsx
@@ -13,15 +13,15 @@ const FoundTireBox = () => {
     return (
         <Tabs.Root defaultValue="foundByVehicle" maxW={"1214px"} bg={"bg"} rounded={"24px"} overflow={"hidden"}>
             <Tabs.List dir={dir} bg="primary" pt="4px">
-                <Tabs.Trigger value="foundByVehicle" bg="white" flex="1" justifyContent={"center"} _selected={{ bg: "primary", color: "white", "--indicator-color": "transparent" }}>
+                <Tabs.Trigger value="foundByVehicle" bg="white" flex="1" justifyContent={"center"} h={"auto"} py={{ base: "2px", md: "8px", xl: "12px" }} _selected={{ bg: "primary", color: "white", "--indicator-color": "transparent" }}>
                     {t("searchByVehicle")}
                 </Tabs.Trigger>
-                <Tabs.Trigger value="foundBySize" bg="white" flex="1" justifyContent={"center"} _selected={{ bg: "primary", color: "white", "--indicator-color": "transparent" }}>
+                <Tabs.Trigger value="foundBySize" bg="white" flex="1" justifyContent={"center"} h={"auto"} py={{ base: "2px", md: "8px", xl: "12px" }} _selected={{ bg: "primary", color: "white", "--indicator-color": "transparent" }}>
                     {t("searchBySize")}
                 </Tabs.Trigger>
             </Tabs.List>
-            <Tabs.Content value="foundByVehicle" py={"32px"} px={"31px"} dir={dir}><FoundByVehicle /></Tabs.Content>
-            <Tabs.Content value="foundBySize" py={"32px"} px={"31px"} dir={dir}><FoundBySize /></Tabs.Content>
+            <Tabs.Content value="foundByVehicle" p={{ base: "8px", md: "19px", xl: "31px" }} dir={dir}><FoundByVehicle /></Tabs.Content>
+            <Tabs.Content value="foundBySize" p={{ base: "8px", md: "19px", xl: "31px" }} dir={dir}><FoundBySize /></Tabs.Content>
         </Tabs.Root>
     )
 }
@@ -37,11 +37,11 @@ const FoundByVehicle = () => {
 
     return (
         <form onSubmit={onSubmit}>
-            <HStack gap={"24px"} alignItems="end">
-                <DropSelectList list={[{ label: "option1", value: "option1" }]} label={t("category")} placeholder={t("selectCategory")} name="category" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
+            <HStack gap={{ base: "4px", md: "12px", xl: "24px" }} alignItems="end" flexWrap={"wrap"}>
+                <DropSelectList list={[{ label: "option1", value: "option1" }, { label: "option2", value: "option2" }]} label={t("category")} placeholder={t("selectCategory")} name="category" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
                 <DropSelectList list={[{ label: "option1", value: "option1" }]} label={t("year")} placeholder={t("selectYear")} name="year" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
                 <DropSelectList list={[{ label: "option1", value: "option1" }]} label={t("make")} placeholder={t("selectMake")} name="make" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
-                <Button flex={1} rounded={"12px"} type='submit'>{t("findYourTireNow")} <SearchIcon /></Button>
+                <Button rounded={"12px"} type='submit' fontSize={{ base: "12px" }} w={{ base: "full", md: "auto" }}>{t("findYourTireNow")} <SearchIcon /></Button>
             </HStack>
         </form>)
 }
@@ -54,11 +54,11 @@ const FoundBySize = () => {
 
     return (
         <form onSubmit={onSubmit}>
-            <HStack gap={"24px"} alignItems="end">
+            <HStack gap={{ base: "4px", md: "12px", xl: "24px" }} alignItems="end" flexWrap={"wrap"}>
                 <DropSelectList list={[{ label: "option1", value: "option1" }]} label={t("height")} placeholder={t("selectHeight")} name="height" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
                 <DropSelectList list={[{ label: "option1", value: "option1" }]} label={t("width")} placeholder={t("selectWidth")} name="width" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
                 <DropSelectList list={[{ label: "option1", value: "option1" }]} label={t("diameter")} placeholder={t("selectDiameter")} name="diameter" containerProps={{ flex: 1 }} triggerProps={{ rounded: "12px" }} />
-                <Button flex={1} rounded={"12px"} type='submit'>{t("findYourTireNow")} <SearchIcon /></Button>
+                <Button rounded={"12px"} type='submit' w={{ base: "full", md: "auto" }}>{t("findYourTireNow")} <SearchIcon /></Button>
             </HStack>
         </form>)
 }
--- frontend/components/pages/home/HeroSection.tsx
+++ frontend/components/pages/home/HeroSection.tsx
@@ -8,20 +8,23 @@ const HeroSection = async () => {
     const t = await getTranslations("home")
     const local = await getLocale()
     return (
-        <Container ps={"80px"} rounded={"50px"} overflow={"hidden"} pt={"83px"} pb={"22px"} zIndex={1} >
+        <Container rounded={"50px"} overflow={"hidden"} pt={{ base: "22px", md: "43px", xl: "83px" }} pb={"22px"} zIndex={1} >
             <Box bgSize={"cover"} bgImage={"url(/images/heroBg.png)"} position={"absolute"} inset={0} zIndex={-1} >
                 <Box className={`inset-0 absolute ${local === "en" ? "bg-linear-to-r" : "bg-linear-to-l"} from-black via-black/40 to-black/0`} />
             </Box>
-            <VStack gap={"39px"} alignItems={"start"} maxW={"575px"} mb="156px">
-                <Text fontSize={local === "en" ? "75px" : "123px"} fontWeight={"800"} color={"white"} lineHeight={local === "en" ? "85px" : "144px"}>{t("controlStartsWith")} <Span color={"primary"}>{t("theRightTire")}</Span></Text>
-                <Text fontSize={"20px"} color={"white"} maxW={"530px"}>{t("shop,book,andInstall—allInOnePlace,PoweredByTheLatestGlobalTireServiceTechnologies")}</Text>
-                <HStack gap={"16px"}>
-                    <Button variant={"outline"} fontSize={"18px"} py={"20px"} h={"auto"}>{t("watchVideo")}<PlayCircleIcon /></Button>
-                    <Button fontSize={"18px"} py={"20px"} h={"auto"}>{t("chooseYourTireNow")}<LeftArrowIcon rotate={local === "en" ? "180deg" : ""} /></Button>
+            <VStack
+                gap={{ base: "18px", md: "28px", xl: "39px" }}
+                alignItems={"start"} maxW={"575px"}
+                mb={{ base: "44px", md: "70px", xl: "120px", "2xl": "156px" }}>
+                <Text fontSize={local === "en" ? { base: "48px", md: "65px", xl: "75px" } : { base: "52px", sm: "72px", md: "90px", "2xl": "123px" }} fontWeight={"800"} color={"white"} lineHeight={local === "en" ? { base: "48px", md: "75px", xl: "85px" } : { base: "84px", md: "104px", "2xl": "144px" }}>{t("controlStartsWith")} <Span color={"primary"}>{t("theRightTire")}</Span></Text>
+                <Text fontSize={{ base: "14px", md: "16px", xl: "20px" }} color={"white"} maxW={"530px"}>{t("shop,book,andInstall—allInOnePlace,PoweredByTheLatestGlobalTireServiceTechnologies")}</Text>
+                <HStack gap={{ base: "8px", md: "12px", xl: "16px" }}>
+                    <Button variant={"outline"} fontSize={{ base: "14px", xl: "18px" }} py={{ base: "15px", xl: "20pxx" }} px={{ base: "6px", md: "15px", xl: "" }} h={"auto"}>{t("watchVideo")}<PlayCircleIcon /></Button>
+                    <Button fontSize={{ base: "14px", xl: "18px" }} py={{ base: "15px", xl: "20pxx" }} px={{ base: "6px", md: "15px", xl: "" }} h={"auto"}>{t("chooseYourTireNow")}<LeftArrowIcon rotate={local === "en" ? "180deg" : ""} /></Button>
                 </HStack>
             </VStack>
             <FoundTireBox />
-        </Container>
+        </Container >
     )
 }
 
