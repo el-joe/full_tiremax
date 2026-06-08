69542c72674ee8330f99eb7c6fc6a006bd3af484
eng-saeed <eng.saeedsayed@gmail.com>
2026-06-08 03:41:42 +0300

    refactor ContactUsSection and ContactUsForm for improved responsiveness and maintainability

--- frontend/components/shared/ContactUsForm.tsx
+++ frontend/components/shared/ContactUsForm.tsx
@@ -16,21 +16,21 @@ const ContactUsForm = () => {
     })
     return (
         <form onSubmit={onSubmit}>
-            <VStack gap="32px" rounded={"16px"} p="32px" border={"1px solid #E5E7EB"} alignItems={"stretch"} >
-                <HStack gap={"32px"}>
+            <VStack gap={{ base: "9px", md: "22px", xl: "32px" }} rounded={"16px"} p={{ base: "9px", md: "22px", xl: "32px" }} border={"1px solid #E5E7EB"} alignItems={"stretch"} >
+                <HStack gap={{ base: "9px", md: "22px", xl: "32px" }}>
                     <Input label={t("fullName")} placeholder={t("enterYourFullName")} startElement={<UserCircleIcon size={"md"} />} />
                     <Input label={t("phoneNumber")} placeholder={t("phoneNumberPlaceholder")} startElement={<PhoneSignalIcon size={"sm"} color={"gray-2"} />} />
                 </HStack>
-                <Input label={t("inquirySubject")} placeholder={t("inquiryAboutPrices")} w="576px" />
+                <Input label={t("inquirySubject")} placeholder={t("inquiryAboutPrices")} w={{ base: "full", xl: "576px" }} />
                 <Textarea label={t("message")} placeholder={t("howCanWeHelpYouToday")} minH={"160px"} />
-                <HStack justifyContent={"space-between"}>
-                    <HStack>
-                        <Text>{t("orContactImmediatelyVia")}</Text>
+                <HStack justifyContent={"space-between"} gap={{ base: "4px" }}>
+                    <HStack gap={{ base: "1px", md: "8px" }}>
+                        <Text fontSize={{ base: "10px", md: "16px" }}>{t("orContactImmediatelyVia")}</Text>
                         <Link href="/">
-                            <Center bg="#41C452" rounded={"8px"} color={"white"} w="48px" h="48px"><WhatsappLogoIcon size={"xl"} /></Center>
+                            <Center bg="#41C452" rounded={"8px"} color={"white"} w={{ base: "24px", md: "38px", xl: "48px" }} h={{ base: "24px", md: "38px", xl: "48px" }}><WhatsappLogoIcon size={{ base: "sm", md: "lg", xl: "xl" }} /></Center>
                         </Link>
                     </HStack>
-                    <Button fontSize="20px" rounded={"16px"} px="48px" py="20px" type='submit'> <SendMessageIcon /> {t("sendMessage")}</Button>
+                    <Button fontSize={{ base: "13px", md: "16px", xl: "20px" }} rounded={{ base: "11px", md: "16px" }} gap={{ base: "3px", md: "11px" }} px={{ base: "9px", md: "24px", xl: "48px" }} py={{ base: "7px", md: "14px", xl: "20px" }} type='submit'> <SendMessageIcon size={{ base: "xs", md: "sm" }} /> {t("sendMessage")}</Button>
                 </HStack>
             </VStack>
         </form>
--- frontend/components/shared/ContactUsSection.tsx
+++ frontend/components/shared/ContactUsSection.tsx
@@ -7,42 +7,18 @@ import ContactUsForm from './ContactUsForm'
 const ContactUsSection = async () => {
     const t = await getTranslations("home")
     return (
-        <Container bg="white" py="80px" roundedTop={"50px"} overflow={"hidden"}>
-            <VStack alignItems={"center"} gap={"24px"} mb={"48px"}>
-                <Heading as="h2" fontSize={"38px"} fontWeight={"bold"}>{t("contactUs")}</Heading>
-                <Text maxW={"527px"} textAlign={"center"} fontSize="18px">{t("contactUsDescription")}</Text>
+        <Container bg="white" py={{ base: "24px", md: "42px", lg: "80px" }} roundedTop={"50px"} overflow={"hidden"}>
+            <VStack alignItems={"center"} gap={{ base: "8px", md: "14px", lg: "20", xl: "24px" }} mb={{ base: "16px", md: "28px", lg: "40px", xl: "48px" }}>
+                <Heading as="h2" fontSize={{ base: "20px", md: "26px", lg: "32px", xl: "38px" }} fontWeight={"bold"}>{t("contactUs")}</Heading>
+                <Text maxW={"527px"} textAlign={"center"} fontSize={{ base: "12px", md: "16px", lg: "18px" }}>{t("contactUsDescription")}</Text>
                 <Box h="8px" w={"96px"} bg="primary" rounded={"12px"} />
             </VStack>
-            <HStack gap={"48px"} alignItems={"start"}>
-                <VStack flex={1} gap={"24px"} alignItems={"start"}>
-                    <HStack gap={"16px"} rounded={"16px"} p="32px" border={"1px solid #E5E7EB"} w={"full"}>
-                        <Center bg="primary" rounded={"16px"} color={"white"} w="64px" h="64px"><PhoneSignalIcon size={"lg"} /></Center>
-                        <Box>
-                            <Text fontSize={"14px"} fontWeight={"semibold"} color={"gray-2"}>{t("customerServiceNumber")}</Text>
-                            <Text fontSize={"36px"} fontWeight={"extrabold"}>6622</Text>
-                        </Box>
-                    </HStack>
-                    <HStack gap={"16px"} rounded={"16px"} p="32px" border={"1px solid #E5E7EB"} w={"full"}>
-                        <Center bg="primary" rounded={"16px"} color={"white"} w="64px" h="64px"><EmailFastIcon size={"lg"} /></Center>
-                        <Box>
-                            <Text fontSize={"14px"} fontWeight={"semibold"} color={"gray-2"}>{t("email")}</Text>
-                            <Text fontSize={"20px"} fontWeight={"bold"}>support@tiremax.iq</Text>
-                        </Box>
-                    </HStack>
-                    <HStack gap={"16px"} rounded={"16px"} p="32px" border={"1px solid #E5E7EB"} w={"full"}>
-                        <Center bg="primary" rounded={"16px"} color={"white"} w="64px" h="64px"><LocationPinIcon size={"lg"} /></Center>
-                        <Box>
-                            <Text fontSize={"14px"} fontWeight={"semibold"} color={"gray-2"}>{t("headOffice")}</Text>
-                            <Text fontSize={"20px"} fontWeight={"bold"}>{t("headOfficeAddress")}</Text>
-                        </Box>
-                    </HStack>
-                    <HStack gap={"16px"} rounded={"16px"} p="32px" border={"1px solid #E5E7EB"} w={"full"}>
-                        <Center bg="primary" rounded={"16px"} color={"white"} w="64px" h="64px"><LocationPinIcon size={"lg"} /></Center>
-                        <Box>
-                            <Text fontSize={"14px"} fontWeight={"semibold"} color={"gray-2"}>{t("branch2")}</Text>
-                            <Text fontSize={"20px"} fontWeight={"bold"}>{t("branch2Address")}</Text>
-                        </Box>
-                    </HStack>
+            <HStack gap={{ base: "14px", md: "32px", xl: "48px" }} alignItems={{ base: "stretch", md: "start" }} flexDir={{ base: "column", md: "row" }}>
+                <VStack flex={1} gap={{ base: "9px", md: "12px", lg: "18px", xl: "24px" }} alignItems={"stretch"}>
+                    <ContactCardInfo Icon={PhoneSignalIcon} heading={t("customerServiceNumber")} contactInfo={"6622"} contactInfoFontSize='36px' />
+                    <ContactCardInfo Icon={EmailFastIcon} heading={t("email")} contactInfo={"support@tiremax.iq"} />
+                    <ContactCardInfo Icon={LocationPinIcon} heading={t("headOffice")} contactInfo={t("headOfficeAddress")} />
+                    <ContactCardInfo Icon={LocationPinIcon} heading={t("branch2")} contactInfo={t("branch2Address")} />
                 </VStack>
                 <ContactUsForm />
             </HStack>
@@ -52,4 +28,12 @@ const ContactUsSection = async () => {
 
 export default ContactUsSection
 
+const ContactCardInfo = ({ Icon, heading, contactInfo, contactInfoFontSize }: { Icon: React.ElementType, heading: string, contactInfo: string, contactInfoFontSize?: string }) => (
+    <HStack gap={{ base: "8px", md: "12px", lg: "16px" }} rounded={"16px"} p={{ base: "9px", md: "22px", xl: "32px" }} border={"1px solid #E5E7EB"} w={"full"}>
+        <Center bg="primary" rounded={"16px"} color={"white"} w={{ base: "32px", md: "42px", xl: "64px" }} h={{ base: "32px", md: "42px", xl: "64px" }}><Icon size={{ base: "sm", md: "md", xl: "lg" }} /></Center>
+        <Box>
+            <Text fontSize={"14px"} fontWeight={"semibold"} color={"gray-2"}>{heading}</Text>
+            <Text fontSize={{ base: "12px", md: "16px", lg: contactInfoFontSize || "20px" }} fontWeight={"bold"}>{contactInfo}</Text>
+        </Box>
+    </HStack>)
 
